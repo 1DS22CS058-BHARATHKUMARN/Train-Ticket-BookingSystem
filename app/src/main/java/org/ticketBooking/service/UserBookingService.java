cbb8bdee8346a117fb7f5ae7bc1b7b3229e8ad67
Bharath <royalchallengers563134@gmail.com>
2025-07-09 11:45:44 +0530

    second commit

--- app/src/main/java/org/ticketBooking/service/UserBookingService.java
+++ app/src/main/java/org/ticketBooking/service/UserBookingService.java
@@ -1,5 +1,36 @@
 package org.ticketBooking.service;
 
-public class UserBookingService {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.ticketBooking.entities.User;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class UserBookingService {
+    private User user;
+    private List<User> userList;
+    private ObjectMapper objectMapper=new ObjectMapper();
+
+    private static final String USER_FILE_PATH="app/src/main/java/org/ticketBooking/localDB/users.json";
+
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        loadUserListFromFile();
+    }
+    public UserBookingService() throws IOException {
+        loadUserListFromFile();
+    }
+    private void loadUserListFromFile() throws IOException {
+        userList = objectMapper.readValue(new File(USER_FILE_PATH), new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
 }
